898d145f13fbd579905443c1d0bff38f40cb946c
byte127x <62516365+byte127x@users.noreply.github.com>
2026-03-19 00:16:34 -0700

    reads images, renders portals

--- Goime1000MapEditor/Panels/MapViewer.axaml.cs
+++ Goime1000MapEditor/Panels/MapViewer.axaml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Net.Mime;
 using System.Text;
 using System.Windows.Input;
 using Avalonia;
@@ -13,6 +14,7 @@ using Avalonia.Media;
 using Avalonia.Media.Imaging;
 using Avalonia.Platform;
 using Avalonia.Threading;
+using BigGustave;
 using Goime1000MapEditor.Components;
 
 namespace Goime1000MapEditor.Panels;
@@ -181,6 +183,47 @@ public partial class MapViewer : UserControl
         return false;
     }
 
+    public bool ImportImage(Stream stream)
+    {
+        Png image = Png.Open(stream);
+        int NewWidth = image.Width;
+        int NewHeight = image.Height;
+        int[] NewTiles = new int[NewWidth * NewHeight];
+
+        // Compare each pixel to the "color value" of each tile. If there's no match, assume air (index 2) 
+        for (int x = 0; x < image.Width; x++)
+        {
+            for (int y = 0; y < image.Height; y++)
+            {
+                Pixel pixel = image.GetPixel(x, y);
+        
+                int PixelTile = -1;
+                for (int i = 0; i < TileInfo.TilePNGPixels.Length; i++)
+                {
+                    Pixel ComparativePixel = TileInfo.TilePNGPixels[i];
+                    if (ComparativePixel.Equals(pixel))
+                    {
+                        PixelTile = i;
+                        break;
+                    }
+                }
+
+                if (PixelTile == -1)
+                {
+                    PixelTile = 2;
+                }
+
+                NewTiles[(y * NewWidth) + x] = PixelTile;
+            }
+        }
+
+        // Switches the map's tile info to the new information, also clears undo/redo stack
+        NewMap(NewWidth, NewHeight, false);
+        Tiles = NewTiles;
+        
+        return true;
+    }
+
     public async void ExportText(StreamWriter stream)
     {
         // Write header
@@ -269,6 +312,28 @@ public partial class MapViewer : UserControl
                     context.DrawRectangle(
                         TileBrush,
                         outline,
+                        bounds
+                    );
+                }
+            }
+            
+            // Portal markers for reference
+            for (int portalID = 0; portalID < TileInfo.PortalBrushes.Length; portalID++)
+            {
+                IBrush PortalBrush = TileInfo.PortalBrushes[portalID];
+                
+                // Renders portal at TWO locations
+                for (int i = 0; i < 2; i++)
+                {
+                    Point NorthwestInner = TileInfo.PortalLocations[(portalID * 2) + i];
+                    Rect bounds = new Rect(
+                        (NorthwestInner.X + PanX - 1) * TileSize + CenterX, (NorthwestInner.Y + PanY - 1) * TileSize + CenterY,
+                        4 * TileSize, 4 * TileSize
+                    );
+                    
+                    context.DrawRectangle(
+                        PortalBrush,
+                        null,
                         bounds
                     );
                 }
